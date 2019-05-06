87b48ae513ae99721adad1024786436690c02841
farkaspeti <farkas.peti97@gmail.com>
2019-05-06 09:59:25 +0200

    task2/3/4/5 done

--- task1.sql
+++ task1.sql
@@ -1,4 +1,4 @@
 SELECT company_name,product_name FROM suppliers
 LEFT JOIN products
-ON suppliers.supplier_id = products.product_id
+ON suppliers.supplier_id = products.supplier_id
 ORDER BY company_name,product_name ASC;
