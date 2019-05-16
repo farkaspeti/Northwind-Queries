68d2ee2106ae0845c7d7658010ecbc93469f9862
Peter Farkas <44734610+farkaspeti@users.noreply.github.com>
2019-05-16 14:48:53 +0200

    task3/task5 updated

--- task3.sql
+++ task3.sql
@@ -1,6 +1,7 @@
-SELECT company_name AS Company FROM products
+SELECT company_name AS Company,COUNT(DISTINCT product_name) AS NumberOfProducts FROM products
 JOIN suppliers
 ON suppliers.supplier_id = products.supplier_id
 GROUP BY Company 
-HAVING COUNT(product_name) = 5;
+HAVING COUNT(DISTINCT product_name) = 5
+ORDER BY Company;
 
--- task5.sql
+++ task5.sql
@@ -2,6 +2,6 @@ SELECT suppliers.company_name, products.product_name, products.unit_price FROM p
 JOIN suppliers ON suppliers.supplier_id = products.supplier_id
 JOIN (SELECT products.supplier_id, max(products.unit_price) AS unit_price
 FROM products
-GROUP BY products.supplier_id) AS MaxQuerry
-ON products.supplier_id = Q1.supplier_id AND products.unit_price = MaxQuerry.unit_price
+GROUP BY products.supplier_id) AS MaxQuery
+ON products.supplier_id = MaxQuery.supplier_id AND products.unit_price = MaxQuery.unit_price
 ORDER BY unit_price DESC;
