c38071e874ac45b31ce88ffcf03dbf24ad40d1f5
Peter Farkas <44734610+farkaspeti@users.noreply.github.com>
2019-06-12 14:28:50 +0200

    Update task4.sql

--- task4.sql
+++ task4.sql
@@ -1,5 +1,5 @@
 SELECT company_name AS CompanyName,ARRAY_AGG(order_id) as orderArray FROM customers
-FULL JOIN orders
+LEFT JOIN orders
 ON customers.customer_id = orders.customer_id
 GROUP BY CompanyName
 ORDER BY CompanyName ASC;
